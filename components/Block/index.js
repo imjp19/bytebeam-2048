9e7e931665ab1a11c53f0b5792a05fc40dd6d167
Chetan Verma <chetan61verma@gmail.com>
2022-04-28 16:10:44 +0530

    Adding Styling

---
+++ components/Block/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Block = ({ number }) => {
+  return (
+    <div className="bg-slate-500 m-1 h-28 w-28 border-2 flex items-center justify-center text-white text-2xl rounded-lg cursor-default shadow-lg">
+      {number}
+    </div>
+  );
+};
+
+export default Block;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Block from "../components/Block";
 
 export default function Home() {
   const [data, setData] = useState([
@@ -7,5 +8,20 @@ export default function Home() {
     [0, 0, 0, 0],
     [0, 0, 0, 0],
   ]);
-  return <div>{TEST}</div>;
+
+  return (
+    <div className="container mx-auto flex items-center justify-center h-screen">
+      <div className="bg-slate-200 p-5 shadow-xl rounded-lg">
+        {data.map((row, oneIndex) => {
+          return (
+            <div className="flex items-center justify-center" key={oneIndex}>
+              {row.map((digit, index) => (
+                <Block number={digit} key={index} />
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
