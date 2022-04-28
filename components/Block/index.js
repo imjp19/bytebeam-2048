3d2ae2d6ce10f5f3b535e7813c2115bad0e8c386
Chetan Verma <chetan61verma@gmail.com>
2022-04-28 16:52:12 +0530

    adding Swiping and event listener

--- components/Block/index.js
+++ components/Block/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Block = ({ number }) => {
   return (
-    <div className="bg-slate-500 m-1 h-28 w-28 border-2 flex items-center justify-center text-white text-2xl rounded-lg cursor-default shadow-lg">
+    <div className="bg-slate-500 m-1 h-28 w-28 border-2 flex items-center justify-center text-white text-4xl rounded-lg cursor-default shadow-lg">
       {number}
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Block from "../components/Block";
+import cloneDeep from "lodash.clonedeep";
+import { useEvent } from "../util";
+
+import { UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW } from "../contants";
 
 export default function Home() {
   const [data, setData] = useState([
@@ -8,6 +12,243 @@ export default function Home() {
     [0, 0, 0, 0],
     [0, 0, 0, 0],
   ]);
+
+  // Initialize
+  const initialize = () => {
+    let newGrid = cloneDeep(data);
+    addNumber(newGrid);
+    addNumber(newGrid);
+    setData(newGrid);
+  };
+
+  // AddNumber - Add a Block
+  const addNumber = (newGrid) => {
+    let added = false;
+    let gridFull = false;
+    let attempts = 0;
+    while (!added) {
+      if (gridFull) {
+        break;
+      }
+
+      let rand1 = Math.floor(Math.random() * 4);
+      let rand2 = Math.floor(Math.random() * 4);
+      attempts++;
+      if (newGrid[rand1][rand2] === 0) {
+        newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
+        added = true;
+      }
+    }
+  };
+
+  // Swipe Left
+  const swipeLeft = (dummy) => {
+    let oldGrid = data;
+    let newArray = cloneDeep(data);
+
+    for (let i = 0; i < 4; i++) {
+      let b = newArray[i];
+      let slow = 0;
+      let fast = 1;
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1;
+          slow++;
+          continue;
+        }
+        if (b[slow] === 0 && b[fast] === 0) {
+          fast++;
+        } else if (b[slow] === 0 && b[fast] !== 0) {
+          b[slow] = b[fast];
+          b[fast] = 0;
+          fast++;
+        } else if (b[slow] !== 0 && b[fast] === 0) {
+          fast++;
+        } else if (b[slow] !== 0 && b[fast] !== 0) {
+          if (b[slow] === b[fast]) {
+            b[slow] = b[slow] + b[fast];
+            b[fast] = 0;
+            fast = slow + 1;
+            slow++;
+          } else {
+            slow++;
+            fast = slow + 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldGrid) !== JSON.stringify(newArray)) {
+      addNumber(newArray);
+    }
+    if (dummy) {
+      return newArray;
+    } else {
+      setData(newArray);
+    }
+  };
+
+  // Swipe Right
+
+  const swipeRight = (dummy) => {
+    let oldData = data;
+    let newArray = cloneDeep(data);
+
+    for (let i = 3; i >= 0; i--) {
+      let b = newArray[i];
+      let slow = b.length - 1;
+      let fast = slow - 1;
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1;
+          slow--;
+          continue;
+        }
+        if (b[slow] === 0 && b[fast] === 0) {
+          fast--;
+        } else if (b[slow] === 0 && b[fast] !== 0) {
+          b[slow] = b[fast];
+          b[fast] = 0;
+          fast--;
+        } else if (b[slow] !== 0 && b[fast] === 0) {
+          fast--;
+        } else if (b[slow] !== 0 && b[fast] !== 0) {
+          if (b[slow] === b[fast]) {
+            b[slow] = b[slow] + b[fast];
+            b[fast] = 0;
+            fast = slow - 1;
+            slow--;
+          } else {
+            slow--;
+            fast = slow - 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(newArray) !== JSON.stringify(oldData)) {
+      addNumber(newArray);
+    }
+    if (dummy) {
+      return newArray;
+    } else {
+      setData(newArray);
+    }
+  };
+
+  // Swipe Down
+
+  const swipeDown = (dummy) => {
+    let b = cloneDeep(data);
+    let oldData = JSON.parse(JSON.stringify(data));
+    for (let i = 3; i >= 0; i--) {
+      let slow = b.length - 1;
+      let fast = slow - 1;
+      while (slow > 0) {
+        if (fast === -1) {
+          fast = slow - 1;
+          slow--;
+          continue;
+        }
+        if (b[slow][i] === 0 && b[fast][i] === 0) {
+          fast--;
+        } else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+          b[slow][i] = b[fast][i];
+          b[fast][i] = 0;
+          fast--;
+        } else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+          fast--;
+        } else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+          if (b[slow][i] === b[fast][i]) {
+            b[slow][i] = b[slow][i] + b[fast][i];
+            b[fast][i] = 0;
+            fast = slow - 1;
+            slow--;
+          } else {
+            slow--;
+            fast = slow - 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(b) !== JSON.stringify(oldData)) {
+      addNumber(b);
+    }
+    if (dummy) {
+      return b;
+    } else {
+      setData(b);
+    }
+  };
+
+  // Swipe Up
+
+  const swipeUp = (dummy) => {
+    let b = cloneDeep(data);
+    let oldData = JSON.parse(JSON.stringify(data));
+    for (let i = 0; i < 4; i++) {
+      let slow = 0;
+      let fast = 1;
+      while (slow < 4) {
+        if (fast === 4) {
+          fast = slow + 1;
+          slow++;
+          continue;
+        }
+        if (b[slow][i] === 0 && b[fast][i] === 0) {
+          fast++;
+        } else if (b[slow][i] === 0 && b[fast][i] !== 0) {
+          b[slow][i] = b[fast][i];
+          b[fast][i] = 0;
+          fast++;
+        } else if (b[slow][i] !== 0 && b[fast][i] === 0) {
+          fast++;
+        } else if (b[slow][i] !== 0 && b[fast][i] !== 0) {
+          if (b[slow][i] === b[fast][i]) {
+            b[slow][i] = b[slow][i] + b[fast][i];
+            b[fast][i] = 0;
+            fast = slow + 1;
+            slow++;
+          } else {
+            slow++;
+            fast = slow + 1;
+          }
+        }
+      }
+    }
+    if (JSON.stringify(oldData) !== JSON.stringify(b)) {
+      addNumber(b);
+    }
+    if (dummy) {
+      return b;
+    } else {
+      setData(b);
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    switch (event.keyCode) {
+      case UP_ARROW:
+        swipeUp();
+        break;
+      case DOWN_ARROW:
+        swipeDown();
+        break;
+      case LEFT_ARROW:
+        swipeLeft();
+        break;
+      case RIGHT_ARROW:
+        swipeRight();
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    initialize();
+  }, []);
+
+  // Adding Event Listeners
+  useEvent("keydown", handleKeyDown);
 
   return (
     <div className="container mx-auto flex items-center justify-center h-screen">
